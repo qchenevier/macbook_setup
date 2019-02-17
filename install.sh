dbbf69177b9bb563392abe3ad1dcb616e98facc4
qchenevier <qchenevier@users.noreply.github.com>
2019-02-17 15:15:50 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # terminal tools
-brew install git zsh zsh-syntax-highlighting zsh-autosuggestions fzf
-brew cask install font-hack-nerd-font iterm2
+brew install --force git zsh zsh-syntax-highlighting zsh-autosuggestions fzf
+brew tap homebrew/cask-fonts
+brew cask install --force font-hack-nerd-font iterm2
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/.zshrc > $HOME/.zshrc
 
 # coding tools
-brew install node
-brew cask install anaconda atom visual-studio-code
+brew install --force node
+brew cask install --force anaconda atom visual-studio-code
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/configure_atom.sh)"
 
 # various tools
-brew cask install firefox gimp skype slack vlc whatsapp
+brew cask install --force firefox gimp skype slack vlc whatsapp
