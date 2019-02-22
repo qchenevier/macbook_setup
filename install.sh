a0268f66580643f1a794091eb366961e12134a5a
qchenevier <qchenevier@users.noreply.github.com>
2019-02-22 13:58:59 +0100

    more clear install script & added iterm2 config

--- install.sh
+++ install.sh
@@ -1,24 +1,49 @@
 # homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+###############################################
 # terminal tools
-brew install --force git zsh zsh-syntax-highlighting zsh-autosuggestions fzf
+###############################################
+# iterm2 installation with custom fonts
 brew tap homebrew/cask-fonts
 brew cask install --force font-hack-nerd-font iterm2
+
+# iterm2 configuration
+mkdir -p $HOME/.iterm2
+curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/com.googlecode.iterm2.plist > $HOME/.iterm2/com.googlecode.iterm2.plist
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# zsh
+brew install --force git zsh zsh-syntax-highlighting zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/.zshrc > $HOME/.zshrc
-curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/.pylintrc > $HOME/.pylintrc
+
+# fuzzy search in terminal
+brew install --force fzf
 $(brew --prefix)/opt/fzf/install
 
+###############################################
 # coding tools
+###############################################
+# frontend (javascript)
 brew install --force node
-brew cask install --force anaconda atom visual-studio-code
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/configure_atom.sh)"
 
+# backend (python)
+brew cask install --force anaconda
+curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/.pylintrc > $HOME/.pylintrc
+
+# backend (spark)
 brew tap homebrew/cask-versions
 brew cask install --force java8
 brew install --force scala apache-spark
 
+# IDEs
+brew cask install --force atom visual-studio-code
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/qchenevier/macbook_setup/master/configure_atom.sh)"
+
+###############################################
 # various tools
+###############################################
 brew cask install --force firefox
